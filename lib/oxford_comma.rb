9c5bddf3d4723f670afad3a8e810363c149e33aa
John Mark Redding <johnmarkredding@gmail.com>
2018-11-09 01:33:57 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  string = ""
+  if array.size == 1
+    string << ", and " << array.shift.to_s
+  elsif array.size == 2
+    string << array.shift.to_s
+    string << array.shift
+  end
+  puts string
 end
+
+oxford_comma([43])
