bd57cc1892f6d1906b80669418db2c2462130c59
John Mark Redding <johnmarkredding@gmail.com>
2018-11-09 01:35:33 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
   string = ""
   if array.size == 1
-    string << ", and " << array.shift.to_s
+    string << array.shift.to_s
   elsif array.size == 2
     string << array.shift.to_s
     string << array.shift
